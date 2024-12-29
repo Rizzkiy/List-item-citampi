2b684eeaf274da1115742d16cd15e4deef32a662
Rzy <164840679+Rizzkiy@users.noreply.github.com>
2024-12-29 16:14:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function filterData() {
+  const input = document.getElementById("searchBox").value.toLowerCase();
+  const items = document.querySelectorAll("#dataList li");
+
+  items.forEach((item) => {
+    const text = item.textContent.toLowerCase();
+    if (text.includes(input)) {
+      item.classList.remove("hidden");
+    } else {
+      item.classList.add("hidden");
+    }
+  });
+}
