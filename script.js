24b07110760a1297d99ba12c570ce4d83464842d
Rzy <164840679+Rizzkiy@users.noreply.github.com>
2024-12-30 16:12:23 +0700

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function filterData() {
-  const input = document.getElementById("searchBox").value.toLowerCase();
-  const items = document.querySelectorAll("#dataList li");
+    const input = document.getElementById("searchBox").value.toLowerCase();
+    const items = document.querySelectorAll("#dataList li");
 
-  items.forEach((item) => {
-    const text = item.textContent.toLowerCase();
-    if (text.includes(input)) {
-      item.classList.remove("hidden");
-    } else {
-      item.classList.add("hidden");
-    }
-  });
+    items.forEach((item) => {
+        const text = item.textContent.toLowerCase();
+        if (text.includes(input)) {
+            item.classList.remove("hidden");
+        } else {
+            item.classList.add("hidden");
+        }
+    });
 }
